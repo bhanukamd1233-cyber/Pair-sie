e7be351dce73cc880408bb1c191926a682ca9ff7
bhanukamd1233-cyber <bhanukamd1233@gmail.com>
2026-02-13 12:56:22 +0530

    Update mega.js

--- mega.js
+++ mega.js
@@ -1,7 +1,7 @@
 const mega = require("megajs");
 const auth = {
-  email: Bhanukamd1233@gmail.com,
-  password: Bhanuka123@,
+  email:Bhanukamd1233@gmail.com,
+  password:Bhanuka123@,
   userAgent:
     "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/42.0.2311.135 Safari/537.36 Edge/12.246",
 };
@@ -41,3 +41,4 @@ const upload = (data, name) => {
 };
 
 module.exports = { upload };
+
